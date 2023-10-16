32086e2e3a310ec930e6e689fc07beb08d7de4fe
Smoowastaken <shayanmoraghebi@gmail.com>
2023-10-16 09:49:06 -0500

    Refactored MinMax class, removed Runner class
    The main method was moved from the standalone Runner class to the MinMax class, making it self-contained and obsoleting the need for an additional Runner class. Two .xml configuration files were also added for IDE settings and user library customizations, improving project setup process.

--- Projects/MoraghebiShayan_MinMax_101623_pd1/src/MinMax.java
+++ Projects/MoraghebiShayan_MinMax_101623_pd1/src/MinMax.java
@@ -1,8 +1,16 @@
 public class MinMax {
-    public double caclculateY(double x){
+    public static void main(String[] args){
+        double x=-1.01;
+        System.out.println("  x        y        g  ");
+        while(x<=2){
+            x+=0.01;
+            System.out.printf("%5.2f   %6.4f   %6.4f\n",x, calculateY(x), calculateG(x));
+        }
+    }
+    public static double calculateY(double x){
         return (1./3)*Math.pow(x,3)-(1./2)*Math.pow(x,2)+2;
     }
-    public double caclculateG(double x){
+    public static double calculateG(double x){
         return Math.pow(x,2)-x;
     }
 }
