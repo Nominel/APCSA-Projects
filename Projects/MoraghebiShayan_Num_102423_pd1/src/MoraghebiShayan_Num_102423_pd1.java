52dc4b2ddbccbd9e60166e9229dd69bfc35fc8f6
Smoowastaken <shayanmoraghebi@gmail.com>
2023-10-24 09:46:38 -0500

    New project Num Nested for loops formatted with printf

---
+++ Projects/MoraghebiShayan_Num_102423_pd1/src/MoraghebiShayan_Num_102423_pd1.java
@@ -0,0 +1,10 @@
+public class MoraghebiShayan_Num_102423_pd1 {
+    public static void main(String[] args){
+        for(int i = 4;i<24;i+=2){
+            for(int b = 0; b<10;b++){
+                System.out.printf("%3d   ",i-3*b);
+            }
+            System.out.println();
+        }
+    }
+}
