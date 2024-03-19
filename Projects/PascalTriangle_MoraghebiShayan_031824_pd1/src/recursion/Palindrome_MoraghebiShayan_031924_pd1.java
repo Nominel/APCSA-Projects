e0bfd07715dbef478c7976197e260b1c784359d5
Shayan <shayanmoraghebi@gmail.com>
2024-03-19 09:46:11 -0500

    palindrome tester with long test

--- Projects/PascalTriangle_MoraghebiShayan_031824_pd1/src/recursion/Palindrome_MoraghebiShayan_031924_pd1.java
+++ Projects/PascalTriangle_MoraghebiShayan_031824_pd1/src/recursion/Palindrome_MoraghebiShayan_031924_pd1.java
@@ -8,14 +8,15 @@ public class Palindrome_MoraghebiShayan_031924_pd1 {
 		System.out.println(isPalindrome("aqubcdfghbcdaqu", 0, 14, 3));
 		System.out.println(isPalindrome("aqubcdfghbcdaqu", 0, 14, 2));
 		System.out.println(isPalindrome("abakadakadaba",0,12,1));
+		System.out.println(isPalindrome("abcdexxxxxxxxxxxxxxabcde",0,23,5));
 	}
 
 	public static boolean isPalindrome(String str, int start, int end, int n){
-		String first = str.substring(start,start+n);
-		String second = str.substring(end-n+1);
 		if (start+n>end-n)
 			return true;
-		else if(first.equals(second))
+		String first = str.substring(start,start+n);
+		String second = str.substring(end-n+1);
+		if(first.equals(second))
 			return isPalindrome(str.substring(0,end-n+1),start+n,end-n,n);
 		else
 			return false;
