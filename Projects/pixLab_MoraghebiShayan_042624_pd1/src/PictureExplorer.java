7c30805c5b720326ff0edca7b6c5f9bc48fa245c
Shayan <shayanmoraghebi@gmail.com>
2024-04-26 09:36:36 -0500

    Pixlab ap lab

--- Projects/pixLab_MoraghebiShayan_042624_pd1/src/PictureExplorer.java
+++ Projects/pixLab_MoraghebiShayan_042624_pd1/src/PictureExplorer.java
@@ -800,7 +800,7 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("images/its-a-trap.jpg");
+    Picture pix = new Picture("images/3dSaulGoodman.jpg");
     pix.explore();
   }
   
